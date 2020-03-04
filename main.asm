d209acde1e2a512705a04dfa4847ec931f8e8e1c
Edelwud <edelwud@icloud.com>
2020-03-04 04:33:28 +0300

    sio int support

--- main.asm
+++ main.asm
@@ -5,14 +5,17 @@ include sio.asm
 
 .data
     message db 80, 0, 79 dup (?), "$"
-
+    integer dw ?
 .code
 start:
     mov ax, @data
     mov ds, ax
 
-    call getstring pascal, offset message
-    call printstring pascal, offset message
+    ; call getstring pascal, offset message
+    ; call printstring pascal, offset message
+    
+    call getint
+    call printint pascal, ax
 exit:
     mov ah, 4ch
     int 21h
--- sio.asm
+++ sio.asm
@@ -19,7 +19,6 @@ endp
 ; | output:
 ; al - printed char
 getchar proc pascal near
-arg char:byte
 uses bx
     push ax
     mov ah, 01h
@@ -60,5 +59,62 @@ uses ax, bx
     xor bh, bh
     mov byte ptr[bx + 2], "$"
     add dx, 2
+    ret
+endp
+
+; Getting integer from console
+; | output:
+; ax - integer
+getint proc pascal near
+arg number:word
+uses cx
+    xor ax, ax
+    xor cx, cx
+    mov ch, 10
+getint_iterator:
+    push ax
+    call getchar
+    cmp al, 0dh
+    je getint_break
+    sub al, "0"
+    mov cl, al
+    pop ax
+    mul ch
+    add al, cl
+    jmp getint_iterator
+getint_break:
+    pop ax
+    ret
+endp
+
+; Print integer
+; | input:
+; number - integer
+printint proc pascal near
+arg number:word
+uses ax, bx, cx, dx
+    mov ax, number
+    mov bl, 10
+    xor cx, cx
+    xor dx, dx
+printint_iterator:
+    div bl
+    inc cx
+    
+    mov dl, ah
+    push dx
+
+    cmp al, 0
+    je printint_break
+    
+    xor ah, ah
+
+    jmp printint_iterator
+printint_break:
+    pop ax
+    xor ah, ah
+    add ax, 30h
+    call putchar pascal, ax
+    loop printint_break
     ret
 endp
